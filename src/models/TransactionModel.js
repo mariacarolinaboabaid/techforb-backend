376e60c8761b1acf4c823d14c1b55db4bb2946ff
@mariacarolinaboabaid <carolkboabaid@gmail.com>
2023-09-05 20:33:51 -0300

    commit

--- src/models/TransactionModel.js
+++ src/models/TransactionModel.js
@@ -54,19 +54,3 @@ Transaction.init(
 
 module.exports = Transaction;
 
-// const getAll = async () => {
-//     const transactions = await connectionDB.execute('SELECT * FROM Transactions');
-//     return transactions[0];
-// };
-
-// const createTransaction = async (userId, transaction) => {
-//     const { value, date, type, description } = transaction;
-//     const queryCreate = 'INSERT INTO Transactions(value, date, type, description, userId) VALUES (?, ?, ?, ?, ?)';
-//     const [createdTransaction] = await connectionDB.execute(queryCreate, [value, date, type, description || "None", userId]);
-//     return { insertId: createdTransaction.insertId };
-// }
-
-// module.exports = {
-//     getAll,
-//     createTransaction
-// }
